ee8b57edb5272089be60f13bd9e3b0a54430cadf
yelnats10 <yelnats10@gmail.com>
2018-01-05 20:41:42 -0500

    reverting back

--- server.js
+++ server.js
@@ -24,7 +24,9 @@ app.use(express.static("public"));
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI, {
+  useMongoClient: true
+});
 
 // Routes
 
